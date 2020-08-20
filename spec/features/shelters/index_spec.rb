2812c3b10dc43af07d3586c63347bd1f83fb533a
Priya Power <priyajag72@gmail.com>
2020-08-20 07:51:22 -0600

    Update shelters index_spec for passing test

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "can see all shetler names" do
-    shelter_1 = Shelter.create(name: "The Humane Society - Denver")
-    shelter_2 = Shelter.create(name: "Denver Animal Shelter")
+  describe "User" do
+    before :each do
+      @shelter_1 = Shelter.create!(name: "The Humane Society - Denver",
+        address: "1 Place St",
+        city: "Denver",
+        state: "CO",
+        zip: "11111")
+        @shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+          address: "7 There Blvd",
+          city: "Denver",
+          state: "CO",
+          zip: "22222")
+    end
 
-    visit "/shelters"
-
-    expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content(shelter_2.name)
+    it "can see all shelter names" do
+      visit "/shelters"
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+    end
   end
 end
