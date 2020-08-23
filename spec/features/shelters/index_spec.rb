2edf5de9f21d427f0b4f15e764de20505ad2cff1
Priya Power <priyajag72@gmail.com>
2020-08-23 14:53:59 -0600

    Update shelter index with link for name

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -32,5 +32,19 @@ RSpec.describe "Shelters Index", type: :feature do
       first(:link, 'Edit Shelter').click
       expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
     end
+
+    it "can link to unique shelter page any time you see shelter name" do
+      visit "/shelters"
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+
+      expect(page).to have_link("#{@shelter_1.name}")
+
+      click_link "#{@shelter_1.name}"
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+
+      expect(page).to have_content("#{@shelter_1.name}")
+      expect(page).to_not have_content("#{@shelter_2.name}")
+    end
   end
 end
